499df53ca70c9b93bc83eeede70a2bbcacee74a8
dejmedus <email96123@gmail.com>
2023-04-06 22:38:30 -0600

    fetch def of random word

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const formEl = document.getElementById('form');
 const defEl = document.getElementById('definition');
 
@@ -9,54 +7,74 @@ formEl.addEventListener('submit', (e) => {
     const formData = new FormData(e.target);
     const word = formData.get('word');
 
+    // set defEl to the definition of a word similar to the form input word
     (async () => {
-        let definition = await getUselessDefinition(word);
-        defEl.innerText = definition !== undefined ? definition : 'No results...'
+        let definition = await fetchRandomWord(word);
+        defEl.innerText = definition;
     })()
 })
 
-async function getUselessDefinition(word) {
-    const definitions = await fetchAntonymDefinitions(word)
-    return definitions[getRandom(definitions.length)]
-}
-
-async function fetchAntonymDefinitions(word) {
-    // fetch word
-    const noResults = ['No results...'];
+async function fetchRandomWord(word) {
+    const firstLetter = word[0].toLowerCase();
+    const wordLength = word.length;
     try {
-        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-        const data = await response.json();
+        // while randomWord is equal to the form input word...
+        let randomWord = [word];
+        while (randomWord[0] == word) {
+            // ...fetch random word that is the same length and starts with the same letter 
+            const response = await fetch(`https://random-word-api.vercel.app/api?words=1&length=${wordLength}&letter=${firstLetter}`)
+            randomWord = await response.json();
 
-        if (data.title == 'No Definitions Found') {
-            return noResults
+            //  get definition of random word
+            return await fetchDefinition(randomWord);
         }
-
-        // fetch list of antonyms
-        for (let i = 0; i < data[0].meanings.length; i++) {
-            const antonym_list = data[0].meanings[i].antonyms;
-
-            if (antonym_list.length > 0) {
-                for (let j = 0; i < antonym_list.length; j++) {
-                    // get antonym definitions array
-                    try {
-                        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${antonym_list[i]}`);
-                        const data = await response.json();
-                        return data[0].meanings[0].definitions.map(def => def.definition)
-                    }
-                    catch {
-                        console.log('Could not find the definition of ', antonym_list[i]);
-                    }
-
-                }
-            }
-        }
-        return noResults
     }
     catch {
-        return noResults
+        // if random word of same length/first letter cannot be found
+        // get an entirely random fallback word and fetch its definition
+        randomWord = await fetchFallbackWord()
+        return await fetchDefinition(randomWord);
     }
+}
+
+async function fetchDefinition(word) {
+    try {
+        // fetch definition array
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const data = await response.json();
+        // choose one random definition
+        const randomDefinition = arrRandom(arrRandom(data[0].meanings).definitions)
+
+        // if that random definition is undefined, try a different random word
+        if (randomDefinition == undefined) {
+            return await fetchRandomWord(word[0])
+        }
+        return randomDefinition.definition
+    }
+    catch {
+        // if a definition cannot be found, try a different random word
+        return await fetchRandomWord(word[0])
+    }
+}
+
+async function fetchFallbackWord() {
+    try {
+        // fetch a random word
+        const response = await fetch(`https://random-word-api.vercel.app/api?words=1`)
+        return await response.json();
+    }
+    catch {
+        // if all fails, use provided word
+        return 'mistake'
+    }
+}
+
+function arrRandom(arr) {
+    // get random array element
+    return arr[getRandom(arr.length)]
 }
 
 function getRandom(len) {
+    // get random number
     return Math.round(Math.random() * len);
 }
